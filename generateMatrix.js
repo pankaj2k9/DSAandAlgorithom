68d1a6ab365dee789b691e0b47ba6ab6c7540293
pankaj2k9 <pkp2.me2k9@gmail.com>
2023-02-26 18:50:51 +0600

    done with spiral matrix

--- generateMatrix.js
+++ generateMatrix.js
@@ -1,6 +1,40 @@
 const generateSpiralMatrix = (n) => {
+    const results = []
+    for(let i = 0; i < n ; i++) {
+       results.push([])
+    }
+    let startColumn = 0;
+    let endColumn = n - 1;
+    let startRow = 0;
+    let endRow = n - 1;
+    let counter = 1;
+    while(startColumn <= endColumn && startRow <= endRow) {
+        for (let i = startColumn; i <= endColumn; i++){
+            results[startRow][i]  = counter;
+            counter++;
+        }
 
+        startRow++;
 
+        for (let i = startRow; i <= endRow; i++){
+            results[i][endColumn]  = counter;
+            counter++;
+        }
+        endColumn--;
+        for(let i = endColumn; i >= startColumn; i--) {
+            results[endRow][i] = counter;
+            counter++;
+        }
+        endRow--;
+        for(let i = endRow; i >= startRow; i--){
+            results[i][startColumn] = counter;
+            counter++;
+        }
+
+        startColumn++
+    }
+
+    return results;
 }
 
-console.log(generateSpiralMatrix([1, 2, 3 , 4, 5, 6, 7, 8, 9]))
+console.log(generateSpiralMatrix(3))
